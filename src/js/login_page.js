cf54d9d1f7a9b6fbe375b1db9a3e3545729096e6
Alexey Klopov <34196081+alexeyklopov@users.noreply.github.com>
2018-04-11 01:31:36 +0300

    FOR INDEX.HTML

---
+++ src/js/login_page.js
@@ -0,0 +1,12 @@
+// Вызов функции при нажатии кнопки "Войти" и перенаправление пользователя на его личную страничку
+function login_func(){
+	var login=document.getElementById('login').value,		// Логин, введенныей в форме на index.html
+		password=document.getElementById('password').value, // Пароль
+		ID = get_user_ID(login, password);
+	if (ID != False){
+		redirect_to_input_page(ID); // Перенаправляем пользователя на его личную страничку
+	}
+	else {
+
+	}
+}
