6e4010c91c07a124531088bcaa6cdb6255f17441
Alexey Klopov <34196081+alexeyklopov@users.noreply.github.com>
2018-04-13 23:05:26 +0300

    Update forpage0.js

--- src/js/forpage0.js
+++ src/js/forpage0.js
@@ -1,15 +1,16 @@
 var d = document,
       last_id = 1;
 
-function add_row(){
-      // находим нужную таблицу
+function add_row() {
+      // Находим нужную таблицу
       var tbody = d.getElementById('tasks').getElementsByTagName('tbody')[0];
 
-      // создаем строку таблицы и добавляем ее
+      // Создаем строку таблицы и добавляем ее
       var row = d.createElement("tr");
+      row.id = "rowId"+parseInt(last_id+1);
       tbody.appendChild(row);
 
-      // создаем ячейки в вышесозданной строке
+      // Создаем ячейки в вышесозданной строке
       var td2 = d.createElement("td");
       var td3 = d.createElement("td");
       var td4 = d.createElement("td");
@@ -23,22 +24,36 @@ function add_row(){
       row.appendChild(td5);
       row.appendChild(td6);
 
-      last_id = last_id + 1;
+      last_id += 1;
 
       td2.innerHTML = last_id;
       td3.innerHTML = '<select><option>Пары</option><option>Еда</option></select>';
       td4.innerHTML = '<input type="range" min="1" max="3" step="1">';
       td5.innerHTML = '<input type="range" min="10" max="18" step="1">';
-      td6.innerHTML = '<button id="'+ parseInt(last_id) +'" class="button_delete" onClick="remove_row(this.id)">X</button>';
+      td6.innerHTML = '<button id="'+'butId'+parseInt(last_id)+'" class="button_delete" onClick="remove_row(this.id)">X</button>';
 }
 
 function remove_row(buttonIdToRemove) {
-      // находим нужную таблицу
+      idToRemove = Number(buttonIdToRemove.slice(5,20));
+      rowIdToRemove = 'rowId'+idToRemove;
+
+      // Находим нужную таблицу
       var tbody = d.getElementById('tasks').getElementsByTagName('tbody')[0];
-      //var tbody.removeChild(buttonIdToRemove);
-      var toRemove = d.getElementById('tasks').getElementsByTagName('tr')[buttonIdToRemove];
-      console.log(toRemove);
+      // В таблице удаляем строку с id "buttonIdToRemove"
+      var toRemove = d.getElementById('tasks').getElementsByTagName('tr')[idToRemove];
+      // Удаляем нужную строку
       tbody.removeChild(toRemove);
-      td2.innerHTML = last_id+1;
-      console.log(last_id);
+
+      // Обновляем индексы кнопок
+      for (var i=idToRemove+1; i<=last_id; i++){ // Пройдемся по строкам,
+      // следущими за buttonIdToRemove, и перенесем id каждого на единицу вверх (уменьшим id)
+            d.getElementById('butId'+parseInt(i)).id = 'butId'+parseInt(i-1);
+      // Обновляем индексы строк в таблице
+            d.getElementById('rowId'+parseInt(i)).id = 'rowId'+parseInt(i-1);
+      // А теперь обновим числа в столбце "#":
+            var nextToRemove = d.getElementById('tasks').getElementsByTagName('tr')[i-1].getElementsByTagName('td')[0];
+            nextToRemove.innerHTML = parseInt(i-1);
+      }
+      
+      last_id -= 1;
 }
