9077396ddd75ba958f56c25a20e1de6d9d22cfce
Alexey Klopov <34196081+alexeyklopov@users.noreply.github.com>
2018-04-15 00:43:34 +0300

    Update login_page.js

--- src/js/login_page.js
+++ src/js/login_page.js
@@ -2,7 +2,7 @@
 function login_func() {
 	var login=document.getElementById('login').value,		// Логин, введенныей в форме на index.html
 		password=document.getElementById('password').value, // Пароль
-		ID = get_user_ID(login, password);
+		ID = get_group_ID(login, password);
 	if (ID != -1) {
 		redirect_to_input_page(ID); // Перенаправляем пользователя на его личную страничку
 	}
