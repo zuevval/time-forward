600d538f2f5a28ba956388234e94fd5744699a31
Valerii Zuev <valera.zuev.zva@gmail.com>
2018-04-13 23:20:12 -0700

    добавил get_schedule
    на этом базовая часть моей работы закончена

--- db_calls/db_calls.js
+++ db_calls/db_calls.js
@@ -7,28 +7,32 @@ function main () {
 	check_if_logged_in();
 	var login=getCookie('login');
 	var pass=getCookie('pass');
-	var raw_events=get_raw_events(login,pass,0);
+	var today=0;
+	var tomorrow=1;
+	var raw_events=get_raw_events(login,pass,today);
 	console.log('raw events are '+raw_events);
 	var group_id=get_group_id(login,pass);
 	console.log('group id is '+group_id);
-	var lesson_start=get_lesson_start(201,group_id,0);
+	var lesson_start=get_lesson_start(201,group_id,today);
 	//возвращает номер пятиминутки
 	console.log('lesson starts at '+lesson_start);
-	var lesson_stop=get_lesson_stop(201,group_id,0);
+	var lesson_stop=get_lesson_stop(201,group_id,today);
 	//возвращает номер пятиминутки
 	console.log('lesson ends at '+lesson_stop);
-	var lesson_name=get_lesson_name(201,group_id,0);
+	var lesson_name=get_lesson_name(201,group_id,today);
 	console.log("lesson is "+lesson_name);
 	var onl=get_nearest_online(login,pass);
 	console.log('nearest online is '+onl);
 	get_online(login,pass,213);
 	var evnts = [201, 202, 203];
-	write_raw_events(login,pass,1 ,evnts);
+	write_raw_events(login,pass,tomorrow,evnts);
 	var schedule= [['история',120,140]];
 	schedule[1]=['матмоделирование',144,164];
 	schedule[2]=['физкультура',168,188];
 	//console.log(schedule);
-	write_schedule(login,pass,1,schedule);
+	write_schedule(login,pass,tomorrow,schedule);
+	var s=get_schedule(login,pass,tomorrow);
+	console.log(s);
 	button1.onclick=function(login,pass){
 		redirect_to_input_page(login,pass);
 	}
@@ -159,7 +163,43 @@ function get_nearest_online(login,pass){
     return online.slice(0,5);
 }
 
+function get_schedule(login,pass,day){
+	//пока что будет работать неправильно, если
+	//расписания ещё нет
+	//пока возвращает одномерный массив-строку,
+	//в которой последовательно записаны событие, начало, конец
+	document.cookie = "pass="+pass;
+	document.cookie = "login="+login;
+	document.cookie = "day="+day;
+	var schedule = [];
+    var oReq = new XMLHttpRequest();
+    oReq.onload = function() {
+		//console.log(this.responseText);
+		var resp=this.responseText;
+		console.log(resp);
+		resp = resp.replace('0', '');
+		var res = resp.split('*');
+		res = res.slice(1,res.length-1);
+		var flag=0;
+		for (var i=0; i<res.length; i++){
+			res[i]=res[i].trim();
+			if(i%3!=0)
+				res[i]=Number(res[i]);
+			if((res[i]==-1||res[i]==['-1'])&&flag==0)
+				flag=i;
+		}
+		res = res.slice(0,flag);
+		console.log(res);
+		events=res;
+    };
+    oReq.open("get", "php/get_schedule.php", false);
+    oReq.send();
+    return events;
+}
+
 function get_raw_events(login,pass,day){
+	//пока что будет работать неправильно, если
+	//событий ещё нет
 	document.cookie = "pass="+pass;
 	document.cookie = "login="+login;
 	document.cookie = "day="+day;
