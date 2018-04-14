c75ad7c8266ce536277a8d86d577248c8f5d7421
Alexey Klopov <34196081+alexeyklopov@users.noreply.github.com>
2018-04-14 13:05:22 +0300

    Update forpage0.js

--- src/js/forpage0.js
+++ src/js/forpage0.js
@@ -1,9 +1,13 @@
-// При открытии страницы выполняем:
+/* Раскомментировать
 login=getCookie('login');
 pass=getCookie('pass');
 
-function loginpass() {
-      if (login==-1||pass==-1) window.alert("Пожалуйста, авторизуйтесь");
+var day = 0,
+      usr_id = get_id_from_url(),
+      events_id = get_raw_events_id(usr_id, day);
+*/
+function loginpass() { // При открытии страницы выполняем:
+      // if (login==-1||pass==-1) window.alert("Пожалуйста, авторизуйтесь");
       // Здесь должен быть редирект на страницу логина
 }
 
@@ -25,7 +29,6 @@ function add_row() {
       var td4 = d.createElement("td");
       var td5 = d.createElement("td");
       var td6 = d.createElement("td");
-
       
       row.appendChild(td2);
       row.appendChild(td3);
@@ -35,11 +38,21 @@ function add_row() {
 
       last_id += 1;
 
+      // events_for_choosing = raw_events_table(events_id) // Массив всех возможных задач
+      events_for_choosing = ['матан', 'линал', 'физра', 'матмод', 'физика', 'спм'];
+      // Создадим меню выбора:
+      var select = d.createElement("select");
+      for (var i = 0; i < events_for_choosing.length; i++) {
+            var option = d.createElement("option");
+            option.text = events_for_choosing[i];
+            select.appendChild(option);
+      }
+      td3.appendChild(select);
+
       td2.innerHTML = last_id;
-      td3.innerHTML = '<select><option>Пары</option><option>Еда</option></select>';
       td4.innerHTML = '<input type="range" min="1" max="3" step="1">';
       td5.innerHTML = '<input type="range" min="10" max="18" step="1">';
-      td6.innerHTML = '<button id="'+'butId'+parseInt(last_id)+'" class="button_delete" onClick="remove_row(this.id)">X</button>';
+      td6.innerHTML = '<button id="butId'+parseInt(last_id)+'" class="button_delete" onClick="remove_row(this.id)">X</button>';
 }
 
 function remove_row(buttonIdToRemove) {
@@ -67,19 +80,34 @@ function remove_row(buttonIdToRemove) {
       last_id -= 1;
 }
 
-
-usr_id = get_id_from_url();
-events_id = get_raw_events_id(usr_id, 0);
-
 function raw_events_table(events_id) {
-      // Возвращает расписание
-      get_online(user_id,online_id);
-      get_lesson_name(lesson_id,group_id);
-      get_elective_name(elective_id,day);
+      // Возвращает названия задач, чтобы пользователь смог добавить задачи в расписание 
+      onlines = []; // Массив названий онлайн-дисциплин
+      lesson_names = []; // Массив названий учебных офлайн-занятий
+      elective_names = []; // Массив названий учебных дополнительных офлайн-занятий
+
+      for (var i = 0; i < events_id.length; i+=2) {
+            onlines.push(get_online(login, pass, events_id[i]));
+      }
+
+      for (var i = 0; i < events_id.length; i++) {
+            lesson_names.push(get_lesson_name(events_id[i], group_id));
+            elective_names.push(get_elective_name(events_id[i], day));
+      }
+
+      /*
+      Использовал функции:
+      get_online(login, pass, online_id);
+      get_lesson_name(lesson_id, group_id);
+      get_elective_name(elective_id, day);
+      */
+
+      var events_for_choosing = onlines+lesson_names+elective_names;
+      return events_for_choosing;
 }
 
 function form_schedule() {
-      calc(usr_id,day);
+      calc(usr_id, day);
       redirect_to_output_page(usr_id);
-      write_raw_events_id(events_id,day);
+      write_raw_events_id(events_id, day);
 }
