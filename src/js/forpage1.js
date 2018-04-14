4dcbf4b163d9600edb83e39665a3b475987200bd
Alexey Klopov <34196081+alexeyklopov@users.noreply.github.com>
2018-04-14 17:26:00 +0300

    Update forpage1.js

--- src/js/forpage1.js
+++ src/js/forpage1.js
@@ -10,16 +10,17 @@ var day = 0,
 var d = document,
       res = [['матан', '12.00', '13.40'], ['линал', '14.00', '15.40'],['спм', '16.00', '17.40']];
 
-function init() { // При открытии страницы выполняем:
-      // if (login==-1||pass==-1) window.alert("Пожалуйста, авторизуйтесь");
-            // Здесь должен быть редирект на страницу логина
-}
 
 // Заполним расписание списком res:
-var tbody = d.getElementById('tbody');
-for (var i = 0; i < res.length; i++) {
+
+/**/
+function init() { // После открытия страницы выполняем:
+      // if (login==-1||pass==-1) window.alert("Пожалуйста, авторизуйтесь");
+      // Здесь должен быть редирект на страницу логина
+
+      var tbody = d.getElementById('tbody');
+      for (var i = 0; i < res.length; i++) {
       var row = d.createElement('tr');
-      console.log(tbody);
       tbody.appendChild(row);
       var td1 = d.createElement('td'),
             td2 = d.createElement('td'),
@@ -28,7 +29,7 @@ for (var i = 0; i < res.length; i++) {
       row.appendChild(td2);
       row.appendChild(td3);
       td1.innerHTML = res[i][0];
-      td1.innerHTML = res[i][1];
-      td1.innerHTML = res[i][2];
-console.log(tbody);
+      td2.innerHTML = res[i][1];
+      td3.innerHTML = res[i][2];
+      }
 }
