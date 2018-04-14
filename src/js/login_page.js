3bc0bbf317c8df44ad52b0d0104b9d0e8bbe8809
Alexey Klopov <34196081+alexeyklopov@users.noreply.github.com>
2018-04-14 11:35:55 +0300

    Update login_page.js

--- src/js/login_page.js
+++ src/js/login_page.js
@@ -1,12 +1,12 @@
 // Вызов функции при нажатии кнопки "Войти" и перенаправление пользователя на его личную страничку
-function login_func(){
+function login_func() {
 	var login=document.getElementById('login').value,		// Логин, введенныей в форме на index.html
 		password=document.getElementById('password').value, // Пароль
 		ID = get_user_ID(login, password);
-	if (ID != false){
+	if (ID != -1) {
 		redirect_to_input_page(ID); // Перенаправляем пользователя на его личную страничку
 	}
-	else {
-		window.location.reload(true); // Обновляем страницу
+	else if (ID == -1) {
+		window.location.reload(true); // Обновление страницы
 	}
 }
