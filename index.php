87264b199d9cca5f5118dcacba3b9640a6669ddb
Valerii Zuev <valera.zuev.zva@gmail.com>
2018-04-16 01:24:03 -0700

    Начал объединять со своим кодом

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
+<?php
+?>
+
+<script type="text/javascript" src="javascript/db_calls.js"></script>
+<script type="text/javascript" src="javascript/index_page.js"></script>
+
+<!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css"> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/index.css">
+
 <!DOCTYPE html>
 <html lang="ru">
 
 <head>
     <meta charset="UTF-8">
     <title>Задачи на день</title>
-    <script src="javascript/index_page.js"></script>
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css"> 
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="css/index.css">
 </head>
 
 <body onload="init()">
@@ -55,5 +60,4 @@
 		</div>
 	</div>
 </body>
-
 </html>
