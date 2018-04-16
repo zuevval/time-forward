a7fc28a0aef262ab00cfeeb1bfbee71644ac052d
Valerii Zuev <valera.zuev.zva@gmail.com>
2018-04-15 23:13:20 -0700

    Изменил структуру сайта
    Кстати, не знаете, как удалить сразу много файлов на Github?

---
+++ javascript/login_page.js
@@ -0,0 +1,12 @@
+// Вызов функции при нажатии кнопки "Войти" и перенаправление пользователя на его личную страничку
+function login_func() {
+	var login=document.getElementById('login').value,		// Логин, введенныей в форме на index.html
+		password=document.getElementById('password').value, // Пароль
+		ID = get_group_ID(login, password);
+	if (ID != -1) {
+		redirect_to_input_page(ID); // Перенаправляем пользователя на его личную страничку
+	}
+	else if (ID == -1) {
+		window.location.reload(true); // Обновление страницы
+	}
+}
